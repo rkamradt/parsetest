c98ba76fccd644bbce2f1559e78c656f4655d1a3
Randal Kamradt Sr <randysr@kamradtfamily.net>
2014-03-21 18:51:28 -0700

    fix console message

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -9,15 +9,14 @@ var parseRestKey="0dC7DQ9Yd75cNR4gWYSNYvPjyNUgHIzN4HAu4hO9";
 
 $(document).ready(function() {
     getMessages();
-    $("#send").click()(function() {
+    $("#send").click(function() {
         var username = $('input[name=username').attr('value');
         var message = $('input[name=messsage]').attr('value');
         console.log(username);
-        console.log('!'')
         $.ajax({url: 'https://api.parse.com/1/classes/MessageBoard', 
             headers: {
                 'X-Parse-Application-Id': parseId,
-                'X-Parse-REST-API=Key': parseRestkey
+                'X-Parse-REST-API=Key': parseRestKey
                 
             }, 
             contentType: 'application/json',
@@ -49,7 +48,7 @@ function getMessages() {
         type: 'GET',
         success: function(data) {
             console.log('get');
-            updateData('data');
+            updateView('data');
         },
         error: function() {
             console.log('error');
